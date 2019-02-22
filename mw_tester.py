9009e25d6a3d02bfc794f33dd7b4fece793e6d84
Taylor Cooke <taylorcooke@gmail.com>
2019-02-22 14:22:31 -0500

    add lots of pieces

---
+++ mw_tester.py
@@ -0,0 +1,52 @@
+import random
+
+word = "MAGNITUDE"
+current_guesses = ["g", "e", "t"]
+def display_letter(letter, guesses):
+    """
+    Conditionally display a letter. If the letter is already in
+    the list `guesses`, then return it. Otherwise, return "_".
+    """
+    if letter in guesses:
+        return letter.upper()
+    else:
+        return "_"
+
+
+
+def print_word(word, guesses):
+    output_letters = [display_letter(letter, guesses) for letter in word]
+    print(" ".join(output_letters))
+    return 
+
+def letter_guess():
+    new_guess = input("Enter your guess (one letter): ")
+    # is 1 letter
+    if len(new_guess) > 1:
+        print("That is too many letters")
+        letter_guess()
+        
+    # check if guess is a letter?
+    all_letters = "abcdefghijklmnopqrstuvwxyz"
+    all_letters += all_letters.upper()
+    # checking if letter already guessed
+    for letter in all_letters:
+        if new_guess not in all_letters:
+            print("This isn't a letter")
+            letter_guess()
+            break
+    # for past in current_guesses:
+    if new_guess in current_guesses:
+        print("Letter already guessed")
+        letter_guess()
+    else:
+        current_guesses.append(new_guess)
+    return new_guess
+
+
+
+
+
+letter_guess()
+letter_guess()
+letter_guess()
--- mystery_word.py
+++ mystery_word.py
@@ -5,12 +5,65 @@ import random
 difficulty = input("Enter your game difficult (easy, hard): ")
 
 # guess letter
-new_guess = input("Enter your guess (one letter): ")
 
+def letter_guess():
+    new_guess = input("Enter your guess (one letter): ")
+    # is 1 letter
+    if len(new_guess) > 1:
+        print("That is too many letters")
+        letter_guess()
+        
+    # check if guess is a letter?
+    all_letters = "abcdefghijklmnopqrstuvwxyz"
+    all_letters += all_letters.upper()
+    # checking if letter already guessed
+    for letter in all_letters:
+        if new_guess not in all_letters:
+            print("This isn't a letter")
+            letter_guess()
+            break
+    
+    if new_guess in current_guesses:
+        print("Letter already guessed")
+        letter_guess()
+    else:
+        current_guesses.append(new_guess)
+    return new_guess
 
+# testing letter_guess
+# past_guess = []
+# def letter_guess():
+#     new_guess = input("Enter your guess (one letter): ")
+#     # is 1 letter
+#     if len(new_guess) > 1:
+#         print("That is too many letters")
+#         letter_guess()
+        
+#     # already guessed list
+#     # past_guess = []
+#     # check if guess is a letter?
+#     all_letters = "abcdefghijklmnopqrstuvwxyz"
+#     all_letters += all_letters.upper()
+#     # checking if letter already guessed
+#     for letter in all_letters:
+#         if new_guess not in all_letters:
+#             print("This isn't a letter")
+#             letter_guess()
+#             break
+#     for past in past_guess:
+#         if new_guess in past_guess:
+#             print("Letter already guessed")
+#             letter_guess()
+            
+#         else:
+#             past_guess.append(new_guess)
+#             return new_guess
 
-def pick_word_easy():
+# find random word - works
+def pick_word():
     # open file
+    # choose difficulty
+    difficulty = input("Enter your game difficult (easy, normal, hard): ")
     with open("words.txt") as file:
         text = file.read()
     # lowercase text
@@ -20,57 +73,80 @@ def pick_word_easy():
 
     # put words in list, may need to use \n instead of .split(), pulling words of correct length
     words = []
-    for word in text.split(" "):
-        if len(word) > 3 and len(word) < 7:
-            words.append(word)
-
+    if difficulty == "easy":
+        for word in text.split(" "):
+            if len(word) > 3 and len(word) < 7:
+                words.append(word)
+    elif difficulty == "normal":
+        for word in text.split(" "):
+            if len(word) > 5 and len(word) < 9:
+                words.append(word)
+    else: 
+        for word in text.split(" "):
+            if len(word) > 7:
+                words.append(word)
     # pick random word for game
-    target = random.choice(words)
-    print(target)
-    return target
+    target_word = random.choice(words)
+    print("The word is ", len(target_word), "letters long.")
+    print(target_word)
+    return target_word
 
-target = pick_word_easy()
+target_word = pick_word()
 
-# display blanks
-print(" _ " * len(target))
+# display blanks - works
+blanks = (" _ " * len(target_word))
+print(blanks)
 
-# how many blanks
-print("The word is ", len(target), "letters long.")
+
 
 # already guessed list
 past_guess = []
 
-# loop for turns
-for turn in range(8):
-    print("Turn", turn + 1)
-    new_guess = input("Enter your guess (one letter): ")
-    # already guessed list
-    past_guess = []
-    # checking if letter already guessed
-    for past in past_guess:
-        if past == new_guess:
-            print("Letter already guessed")
-        else:
-            past_guess.append(new_guess)
+
     
-# from class notes
+
+    
+# from class notes - works
 def display_letter(letter, guesses):
     """
     Conditionally display a letter. If the letter is already in
     the list `guesses`, then return it. Otherwise, return "_".
     """
     if letter in guesses:
-        return letter
+        return letter.upper()
     else:
         return "_"
 
-[display_letter(letter, current_guesses) for letter in word]
+
 
 def print_word(word, guesses):
     output_letters = [display_letter(letter, guesses) for letter in word]
     print(" ".join(output_letters))
+
+
+# game has turns - works
+def run_game():
     
-print_word(word, guesses)
+    display = display_letter(word, current_guesses)
+    print(display)
+    bad_guesses = 0
+    while "_" in display:
+        print("Turn: ", bad_guesses + 1)
+        new_guess = input("Enter your guess (one letter): ")
+        bad_guesses += 1
+        if bad_guesses == 8:
+            print("Game Over")
+            play_again()
+            break
+
+# play again? - works
+def play_again():
+    response = input("Play again? (Y/N): ")
+    if response == "Y" or response == "y":
+        run_game()
+    else:
+        print("OK Goodbye! Thanks for playing!")
+
 # random shit - may not be necessary
 # if __name__ == "__main__":
 #     import argparse
