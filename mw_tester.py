c10ccf379397a4839031c345baea63e88726b722
Taylor Cooke <taylorcooke@gmail.com>
2019-02-25 15:34:11 -0500

    adding testing

--- mw_tester.py
+++ mw_tester.py
@@ -1,169 +1,194 @@
-import random
+# import random
 
+# current_guesses = []
 
-current_guesses = []
-def pick_word():
-    """Asking user for difficulty, creating word list based on response"""
-    # open file
-    # choose difficulty
-    difficulty = input("Enter your game difficult (easy, normal, hard): ")
-    with open("words.txt") as file:
-        text = file.read()
-        file.close
-
-    # # # does with auto-close file?
-    # lowercase text
-    text = text.lower()
-
-    # replace new lines with spaces
-    text = text.replace("\n", " ")
-    difficulty = difficulty.lower()
-
-    # put words in list, may need to use \n instead of .split(), pulling words of correct length
-    words = []
-    if difficulty == "easy":
-        for word in text.split(" "):
-            if len(word) > 3 and len(word) < 7:
-                words.append(word)
-    elif difficulty == "normal":
-        for word in text.split(" "):
-            if len(word) > 5 and len(word) < 9:
-                words.append(word)
-    elif difficulty == "hard":
-        for word in text.split(" "):
-            if len(word) > 7 :
-                words.append(word)
+# def pick_word():
+#     """Asking user for difficulty, creating word list based on response"""
+#     # open file
+#     # choose difficulty
     
-    # make response for invalid response
-    else: 
-        print("Invalid response")
-        pick_word()
+#     with open("words.txt") as file:
+#         text = file.read()
+#         file.close
+
+#     # # # does with auto-close file?
+#     # lowercase text
+#     text = text.lower()
+
+#     # replace new lines with spaces
+#     text = text.replace("\n", " ")
+   
+
+#     # put words in list, may need to use \n instead of .split(), pulling words of correct length
+#     words = []
+#     pick = True
+
+#     # picking letter choice loop
+#     while pick:
+#         difficulty = input("WELCOME TO MYSTERY WORD!!\nEnter your game difficult (easy, normal, hard): ")
+#         difficulty = difficulty.lower()
+#         if difficulty == "easy":
+#             for word in text.split(" "):
+#                 if len(word) > 3 and len(word) < 7:
+#                     words.append(word)
+#             pick = False
+#         elif difficulty == "normal":
+#             for word in text.split(" "):
+#                 if len(word) > 5 and len(word) < 9:
+#                     words.append(word)
+#             pick = False
+#         elif difficulty == "hard":
+#             for word in text.split(" "):
+#                 if len(word) > 7 :
+#                     words.append(word)
+#             pick = False
+        
+#         # make response for invalid response
+#         else: 
+#             print("Invalid response")
+            
     
-    # pick random word for game
-    target_word = random.choice(words)
-    print("The word is ", len(target_word), "letters long.")
-    print(target_word) # for testing
-    return target_word
-
-# final_target_word = pick_word()
-
-def display_letter(letter, guesses):
-    """
-    Conditionally display a letter. If the letter is already in
-    the list `guesses`, then return it. Otherwise, return "_".
-    """
-    if letter in guesses:
-        return letter.upper()
-    else:
-        return "_"
-
-
-
-
-
-
-def letter_guess():
-    """Asks user for letter, validates guess, returns if valid"""
+#     # pick random word for game
+#     target_word = random.choice(words)
+#     print("The word is ", len(target_word), "letters long.")
     
-    # check if guess is a letter
+#     print(target_word) # for testing
+#     return target_word
+
+# def display_letter(letter, guesses):
+#     """
+#     Conditionally display a letter. If the letter is already in
+#     the list `guesses`, then return it. Otherwise, return "_".
+#     """
+#     if letter in guesses:
+#         return letter.upper()
+#     else:
+#         return "_"
+
+
+# def letter_guess():
+#     """Asks user for letter, validates guess, returns if valid"""
+    
+#     # check if guess is a letter
+#     all_letters = "abcdefghijklmnopqrstuvwxyz"
+#     all_letters += all_letters.upper()
+
+#     # checking if entry is valid already guessed
+    
+#     while True:
+#         new_guess = input("Enter your guess (one letter): ")
+#         new_guess = new_guess.lower()
+#         if new_guess in current_guesses:
+#             print("\nLetter already guessed\n")
+            
+#         elif len(new_guess) != 1:
+#             print("\nI need one letter\n")
+            
+#         elif new_guess not in all_letters:
+#             print("\nThis isn't a letter\n")    
+            
+#         else:
+#             current_guesses.append(new_guess)
+#             return new_guess
+
+# breaking out above function for testing
+def is_letter(new_guess):
     all_letters = "abcdefghijklmnopqrstuvwxyz"
     all_letters += all_letters.upper()
+    new_guess = input("Enter your guess (one letter): ")
+    new_guess = new_guess.casefold()
+    if new_guess.lower() in all_letters:
+        return True
 
-    # checking if entry is valid already guessed
+def correct_length(new_guess):
+    if len(new_guess) == 1:
+        return True
+
+def in_current_guesses(new_guess):
+    current_guesses = ['a', 'b', 'c', 'e', 'f']
+    if new_guess.lower() not in current_guesses:
+        return True
+
+# def run_game():
+#     """The game"""
     
-    while True:
-        new_guess = input("Enter your guess (one letter): ")
-        new_guess = new_guess.lower()
-        if new_guess in current_guesses:
-            print("Letter already guessed")
-            
-        elif len(new_guess) != 1:
-            print("I need one letter")
-            
-        elif new_guess not in all_letters:
-            print("This isn't a letter")    
-            
-        else:
-            current_guesses.append(new_guess)
-            return new_guess
-
-
-
-def run_game():
-
-    # clears guesses from previous game
-    del current_guesses[:]
-    final_target_word = pick_word()
-    # display initial blanks
-    print("_ " * len(final_target_word))
-    # find unique letters in final word
-    all_letters = "abcdefghijklmnopqrstuvwxyz"
-    unique_letters_in_final = []
-    for letter in final_target_word:
-        if letter in all_letters and letter not in unique_letters_in_final:
-            unique_letters_in_final.append(letter)
+#     # clears guesses from previous game
+#     del current_guesses[:]
+#     final_target_word = pick_word()
     
-    # displays blanks with guessed letters once game begins
-    def print_word(word, guesses):
-        output_letters = [display_letter(letter, guesses) for letter in final_target_word]
-        print(" ".join(output_letters))
-        return
-    print(unique_letters_in_final) # for testing
-    # print("# unique letter", len(unique_letters_in_final)) # for testing
-    bad_guesses = 0
-    winner_check = []
+#     # display initial blanks
+#     print("_ " * len(final_target_word))
     
-    # can also use for loop to compare guessed letters to word letters
-    # for guess in current_guesses:
-    while True:
-        
-        #showing turn
-        print("Turn: ", bad_guesses + 1)
-        
-        #showing current guesses # for testing
-        print(''.join(current_guesses))
-        print(winner_check)
+#     # find unique letters in final word
+#     all_letters = "abcdefghijklmnopqrstuvwxyz"
+#     unique_letters_in_final = []
+#     for letter in final_target_word:
+#         if letter in all_letters and letter not in unique_letters_in_final:
+#             unique_letters_in_final.append(letter)
+    
+#     # displays blanks with guessed letters once game begins
+#     def print_word(word, guesses):
+#         output_letters = [display_letter(letter, guesses) for letter in final_target_word]
+#         print(" ".join(output_letters))
+#         return
 
-        #running guess letter fxn
-        recent = letter_guess()
-        
-        #printint target word as a check
-        print_word(final_target_word, current_guesses)
+#     # print(unique_letters_in_final) # for testing
+#     # print("# unique letter", len(unique_letters_in_final)) # for testing
+#     bad_guesses = 0
+#     winner_check = []
 
-        # declaring winner
-        # adding correctly guessed letter to a good guess list
-        for letter in final_target_word:
-            if letter in current_guesses and letter not in winner_check:
-                winner_check.append(letter)
-        # comparing len of unique letter lists between word and good guesses
-        if len(unique_letters_in_final) == len(winner_check):
-            print("You win!")
-            play_again()
-            return  
+#     # can also use for loop to compare guessed letters to word letters
+#     # for guess in current_guesses:
+#     while True:
         
-        if recent in final_target_word:
-            print("Good guess!")
-            # current_guesses.append(recent)
-        else:
-            print("Not quite")
-            bad_guesses += 1
+#         #showing turn
+#         print("Turn: ", bad_guesses + 1)
         
+#         # showing current guesses # for testing
+#         print("Your current guesses: ", ''.join(current_guesses))
+#         # print(winner_check) 
+
+#         #running guess letter fxn
+#         recent = letter_guess()
         
+#         #printint target word as a check
+#         print_word(final_target_word, current_guesses)
+
+#         # declaring winner
+#         # adding correctly guessed letter to a good guess list
+#         for letter in final_target_word:
+#             if letter in current_guesses and letter not in winner_check:
+#                 winner_check.append(letter)
+#         # comparing len of unique letter lists between word and good guesses
+#         if len(unique_letters_in_final) == len(winner_check):
+#             print("\nYou win!\n")
+#             play_again()
+#             return  
         
-        #ending game with too many guesses
-        if bad_guesses == 5:
-            print(f"Game Over. The word is {final_target_word}.")
-            play_again()
-            break
+#         if recent in final_target_word:
+#             print("\nGood guess!\n")
+    
+#         else:
+#             print("\nNot quite\n")
+#             bad_guesses += 1
+        
+#         #ending game with too many guesses
+#         if bad_guesses == 8:
+#             print(f"Game Over. The word is {final_target_word}.")
+#             play_again()
+#             break
 
 
-def play_again():
-    """Asks user if they would like to replay game"""
-    response = input("Play again? (Y/N): ")
-    if response == "Y" or response == "y":
-        run_game()
-    else:
-        print("OK Goodbye! Thanks for playing!")
-        return
+# def play_again():
+#     """Asks user if they would like to replay game"""
+#     response = input("Play again? (Y/N): ")
+#     if response.lower() == "y" or response == "yes":
+#         run_game()
+#     else:
+#         print("OK Goodbye! Thanks for playing!")
+#         return
 
-run_game()
+# run_game()
+
+if __name__ == "__main__":
+    pass
--- mystery_tester.py
+++ mystery_tester.py
@@ -1,6 +1,25 @@
-from mystery_word import letter_guess
+from mw_tester import is_letter, correct_length, in_current_guesses
 
 # how to write tests when functions don't take parameters?
 
-def test_letter_guess():
-    assert 
+def test_is_letter():
+    assert is_letter('a')
+    assert is_letter('K')
+    assert not is_letter('9')
+    assert not is_letter('ls')
+    assert is_letter('e')
+
+# def test_correct_length():
+#     assert correct_length('d')
+#     assert correct_length('F')
+#     assert not correct_length('df')
+#     assert not correct_length('KL')
+#     assert not correct_length('')
+
+# def test_in_current_guesses():
+#     assert in_current_guesses('a')
+#     assert in_current_guesses('b')
+#     assert not in_current_guesses('n')
+#     assert not in_current_guesses('9')
+#     assert in_current_guesses('C')
+#     assert not in_current_guesses('V')
